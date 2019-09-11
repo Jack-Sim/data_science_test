07585bd06ed572db116a3af10675505ed2c2a2ef
codeCycler2 <jackrsim1@gmail.com>
2019-09-11 07:51:47 +0100

    created a wordcloud of the text descriptions of the first 20 books from home page

--- web_scrapping/nlp.py
+++ web_scrapping/nlp.py
@@ -1,6 +1,9 @@
 from nltk.corpus import stopwords
 import pandas as pd#
 import nltk
+import matplotlib.pyplot as plt
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 # import the table containing scraped data
 text = pd.read_csv("scraped_data.csv")
@@ -28,3 +31,10 @@ for row in text.iterrows():
     print(len(tokenised_text[link_path]), len(clean_tokens))
     freq = nltk.FreqDist(clean_tokens)
     freq.plot(10, cumulative = False)
+
+    # Generate WordCloud
+    text = " "
+    wordcloud = WordCloud().generate(text.join(clean_tokens))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
